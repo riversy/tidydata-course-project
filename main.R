8f9000ac333e3f036788727188cfda3412cf3c3f
Igor Goltsov <riversy@gmail.com>
2015-04-25 00:17:46 +0300

    Implement # All functionality without Readme and Cookbook

--- main.R
+++ main.R
@@ -7,16 +7,19 @@
 # load necessary libraries
 library(dplyr)
 
-getDirSeparator <- function(){
+###
+# Retrieves system dir deparator (in most of cases it "/") 
+###
+get_dir_separator <- function(){
     "/"
 }
 
 ## 
 # Generate proper path to data file
 ##
-getFilePath <- function(folder, route, id, isInternalSignals = FALSE){
+get_file_path <- function(folder, route, id){
 
-    separator <- getDirSeparator()
+    separator <- get_dir_separator()
 
     path <- paste(
         folder,
@@ -25,16 +28,6 @@ getFilePath <- function(folder, route, id, isInternalSignals = FALSE){
         separator,
         sep = ""
     )
-
-    if (isInternalSignals){
-        
-        path <- paste(
-            path,
-            "Inertial Signals",
-            separator,
-            sep = ""
-        )
-    }
 
     path <- paste(
         path,
@@ -48,69 +41,111 @@ getFilePath <- function(folder, route, id, isInternalSignals = FALSE){
     path
 }
 
-getFeatures <- function(folder){
+###
+# Load descriptive file
+###
+load_descriptive_file <- function(folder, file.name) {
 
-    pathToFile <- paste(
+    path_to_file <- paste(
         folder,
-        getDirSeparator(),
-        "features.txt"
+        get_dir_separator(),
+        file.name
     , sep = "")
 
-    read.table(pathToFile)
+    data <- read.table(path_to_file)
+    names(data) <- c("no", "name")
+    data
 }
 
-getActivityLabels <- function(folder){
-
-    pathToFile <- paste(
-        folder,
-        getDirSeparator(),
-        "activity_labels.txt"
-    , sep = "")
-
-    read.table(pathToFile)
+###
+# Load feature keys
+###
+get_features <- function(folder){
+    load_descriptive_file(folder, "features.txt")
 }
 
-loadData <- function(folder = "UCI HAR Dataset", route = "test"){
+###
+# Load activity label descriptions
+###
+get_activity_labels <- function(folder){
+    load_descriptive_file(folder, "activity_labels.txt")
+}
 
-    assign("activity_labels", getActivityLabels(folder), envir=globalenv())
-    assign("features", getFeatures(folder), envir=globalenv())
+###
+# Retrieves keys to extract from raw dataset
+###
+get_keys_to_extract <- function(folder){
 
-    externalSignalKeys = c("y", "X", "subject")
-    for (id in externalSignalKeys){
-
-        pathToFile <- getFilePath(folder, route, id)
-
-        value = read.table(pathToFile)
-        assign(id, value, envir=globalenv())
-
-        print(id)
-        print(names(value))
-    }
-
-    internalSignalKeys = c(
-        "body_acc_x", 
-        "body_acc_y",
-        "body_acc_z",
-        "body_gyro_x",
-        "body_gyro_y",
-        "body_gyro_z",
-        "total_acc_x",
-        "total_acc_y",
-        "total_acc_z"
+    features <- get_features(folder)
+    filter(
+        features, 
+        grepl('mean\\(\\)|std\\(\\)', name)
     )
-    for (id in internalSignalKeys){
-
-        pathToFile <- getFilePath(folder, route, id, isInternalSignals = TRUE)
-        value = read.table(pathToFile)
-        assign(id, value, envir=globalenv())
-
-        print(id)
-        print(names(value))
-    }
-
-    names(X) <- features$V2
-    assign("X", X, envir=globalenv())
-
 }
 
-loadData()
+###
+# Load and Clear data for some route ("test" or "train")
+###
+load_and_select <- function(folder, route){
+
+    # Load Data into separate datasets
+    external_signal_keys = c("y", "X", "subject")
+    for (id in external_signal_keys){
+
+        path_to_file <- get_file_path(folder, route, id)
+        value = read.table(path_to_file)
+        assign(id, value)
+    }
+
+    # Save only necessary data
+    keys_to_extract <- get_keys_to_extract(folder)
+    X <- select(X, keys_to_extract$no)
+    names(X) <- keys_to_extract$name
+
+    # Append Activity type before data
+    # but factorise it first
+    activity_labels <- get_activity_labels(folder)
+    names(y) <- c("activity")
+    y$activity <- factor(
+        y$activity, 
+        levels = activity_labels$no,
+        labels=activity_labels$name
+    )    
+    X <- bind_cols(y, X)
+
+    # Append Subject Id 
+    names(subject) <- c("subject")
+    X <- bind_cols(subject, X)
+
+    X
+}
+
+###
+# Prepare Tidy Data from Raw dataset
+#
+# @param read.folder
+# @param save.path
+# @param open.view
+###
+prepare_tidy_data <- function(
+    read.folder = "UCI HAR Dataset", 
+    save.path = "uci_har_dataset.txt",
+    open.view = FALSE
+){
+    
+    # Merge selected data into one data frame
+    data <- bind_rows( 
+        load_and_select(read.folder, route = "test"),
+        load_and_select(read.folder, route = "train")
+    ) %>% 
+    group_by(activity, subject) %>%     # Group by activity and subject
+    summarise_each(funs(mean))          # Prepare average values for each of activity <-> subject pair
+
+    # Open data in View() if necessary
+    if (open.view){
+        View(data)
+    }
+
+    # Write it to disk
+    write.table(data, save.path, row.name=FALSE)
+}
